8f97e2c4f6c3c068d1d7ec9723807c28573860a4
Yashraj_K <yashrajkamble7727@gmail.com>
2026-02-26 15:06:39 +0530

    Ref & other features

---
+++ refAndOtherFeatures/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Container from "./components/container";
+
+function App() {
+  return (
+    <div>
+      <h1>React 19 Tutorials</h1>
+      <h2>Refs</h2>
+      <Container />
+    </div>
+  );
+}
+
+export default App;
